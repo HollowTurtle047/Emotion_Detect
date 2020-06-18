2d290352878c799da076d2f870ac3f2c2a02574b
HollowTurtle047 <a1014gong@126.com>
2020-06-18 16:02:10 +0800

    apply recognize facial emotion

---
+++ recognize.py
@@ -0,0 +1,83 @@
+
+import cv2
+# import csv
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models, callbacks
+import numpy as np
+import os
+# from nets import *
+import nets
+
+
+class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+# model = nets.getnet1()
+model = nets.getnet2()
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+""" checkpoint_path1 = "./models_2/model.ckpt"
+checkpoint_dir1 = os.path.dirname(checkpoint_path1)
+latest = tf.train.latest_checkpoint(checkpoint_dir1) """
+
+checkpoint_path2 = "./models_3/model.ckpt"
+checkpoint_dir2 = os.path.dirname(checkpoint_path2)
+latest = tf.train.latest_checkpoint(checkpoint_dir2)
+
+model.load_weights(latest)
+
+
+### Camara
+cap = cv2.VideoCapture(0)
+iter_num = 5
+tempFaces = np.zeros((iter_num,48,48))
+count = 0
+haarcascade_path = '/home/dean/Projects/Emotion_Detect/.venv/lib64/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml'
+detector = cv2.CascadeClassifier(haarcascade_path)
+emotion = 'Neutral'
+while(True):
+    ret,frame = cap.read()
+    
+    ## cv2 detector
+    face_zone = detector.detectMultiScale(frame)
+    cv2.putText(frame,"Q to quit", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0))
+    for x,y,w,h in face_zone:
+        cv2.putText(frame, emotion, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
+        cv2.rectangle(frame, pt1=(x,y), pt2=(x+w,y+h), color=[0,0,255])
+        frame_slice = frame[y:y+h, x:x+w]
+        frame_slice = cv2.resize(frame_slice, (48,48))
+        # cv2.imshow('frame_slice',frame_slice) # enable to see the slice
+        
+        tempFaces[count] = cv2.cvtColor(frame_slice,cv2.COLOR_BGR2GRAY) # gray and save
+        count = (count+1)%iter_num
+        
+        # detection
+        if count == 0:
+            tempFaces = tempFaces / 255.0
+            with tf.device('/cpu:0'):
+                # preds = np.argmax(model.predict(tempFaces), axis=-1)
+                preds = np.argmax(model.predict(np.resize(tempFaces,(iter_num,48,48,1))), axis=-1)
+            counts = np.bincount(preds)
+            label = np.argmax(counts)
+            emotion = class_names[label]
+        
+    cv2.imshow('frame',frame)
+    
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
+    
+
+# with tf.device('/cpu:0'):
+#     for i in range(179):
+#         temp_x = x_test[20*i:20*(i+1)]
+#         preds = np.argmax(model.predict(temp_x), axis=-1)
+#         print(preds)
+#         c = np.bincount(preds)
+#         print(np.argmax(c))
+#         print(y_test[20*i:20*(i+1)])
+#         print('next')
+
--- training.py
+++ training.py
@@ -40,35 +40,32 @@ with open('fer2013.csv') as f:
         #     break
 
 x_train = np.array(train_images)
-x_train = x_train.reshape((28709,48,48))
+x_train = x_train.reshape((28709,48,48,1))
 y_train = np.array(train_labes)
 x_test = np.array(test_images)
-x_test = x_test.reshape(3589,48,48)
+x_test = x_test.reshape((3589,48,48,1))
 y_test = np.array(test_labes)
 
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
-# print(x_train.shape)
-# model = models.Sequential()
-# model.add(layers.Conv2D)
+x_train, x_test = x_train/225.0, x_test/225.0
 
 model = models.Sequential([
-    layers.Convolution1D(32, 5, strides=1, padding='same', input_shape=(48,48), activation='relu'),
-    layers.MaxPooling1D(pool_size=3, strides=2),
-    layers.Convolution1D(32, 4, strides=1, padding='same', activation='relu'),
-    layers.MaxPooling1D(pool_size=3, strides=2),
-    layers.Convolution1D(32, 5, strides=1, padding='same', activation='relu'),
-    layers.MaxPooling1D(pool_size=3, strides=2),
+    layers.Conv2D(32, (5,5), strides=1, padding='same', input_shape=(48,48,1), activation='relu'),
+    layers.MaxPooling2D(pool_size=(3,3), strides=2),
+    layers.Conv2D(32, (4,4), strides=1, padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size=(3,3), strides=2),
+    layers.Conv2D(64, (5,5), strides=1, padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size=(3,3), strides=2),
     layers.Flatten(),
-    layers.Dense(2048,activation='relu'),
+    layers.Dense(2048, activation='relu'),
     layers.Dropout(0.4),
-    layers.Dense(1024,activation='softmax'),
-    layers.Dropout(0.4)
+    layers.Dense(1024, activation='relu'),
+    layers.Dropout(0.4),
+    layers.Dense(7, activation='softmax')
 ])
 
 # model.summary()
 
-model.compile(optimizer='sgd',
+model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
@@ -79,10 +76,6 @@ cp_callback = callbacks.ModelCheckpoint(checkpoint_path, verbose=1, save_weights
 
 # model.save_weights(checkpoint_path.format(epoch=0))
 with tf.device('/cpu:0'):
-    model.fit(x_train, y_train, epochs=1500, callbacks=[cp_callback])
+    model.fit(x_train, y_train, batch_size=100, epochs=20, callbacks=[cp_callback])
     model.evaluate(x_test, y_test)
-
-
-
-
 
